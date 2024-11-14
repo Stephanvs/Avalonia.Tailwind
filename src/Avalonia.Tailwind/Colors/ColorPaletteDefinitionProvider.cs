0921f5ebf43e73371feb8a4eab5543178609c50c
Stephan van Stekelenburg <stephan@hayman.io>
2024-11-14 14:58:11 +0100

    refactor: some new language constructs.
    renamed color namespace to colors

---
+++ src/Avalonia.Tailwind/Colors/ColorPaletteDefinitionProvider.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Media;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Avalonia.Tailwind.Colors;
+
+public interface IColorDefinitionProvider
+{
+  ImmutableArray<(string name, ImmutableArray<Color> colors)> ColorDefinitions { get; }
+}
+
+public class ColorPaletteDefinitionProvider(
+  IEnumerable<(string name, ImmutableArray<Color> colors)> colorDefinitions)
+  : IColorDefinitionProvider
+{
+  public ImmutableArray<(string name, ImmutableArray<Color> colors)> ColorDefinitions { get; } = [..colorDefinitions];
+}
