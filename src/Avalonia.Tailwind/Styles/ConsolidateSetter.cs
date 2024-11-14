13967dbad761a98587e2c861b1c4c96079963c83
Stephan van Stekelenburg <stephan@hayman.io>
2024-11-14 14:41:24 +0100

    feat: replace NaN with 0 to enable layout measurement

--- src/Avalonia.Tailwind/Styles/ConsolidateSetter.cs
+++ src/Avalonia.Tailwind/Styles/ConsolidateSetter.cs
@@ -1,58 +1,34 @@
-﻿using System;
-using Avalonia.Controls;
-using Avalonia.Styling;
+﻿using Avalonia.Styling;
 
 namespace Avalonia.Tailwind
 {
   public class ConsolidateSetter() : Setter
   {
-    private readonly Setter internalSetter = new();
-
-    public new AvaloniaProperty Property { get; set; }
-
-    public new object Value { get; set; }
-
     public ConsolidateSetter(AvaloniaProperty property, object value)
       : this()
     {
-      this.Property = property;
-      this.Value = value;
+      Property = property;
+      Value = GetConsolidatedValue(property, value);
     }
 
-    private static Thickness Merge(StyledElement control, AvaloniaProperty<Thickness> property, Thickness newValue)
-    {
-      var oldValue = control.GetValue<Thickness>(property);
-
-      return new Thickness
-      (
-        left:   double.IsNaN(newValue.Left)   ? oldValue.Left   : newValue.Left,
-        top:    double.IsNaN(newValue.Top)    ? oldValue.Top    : newValue.Top,
-        right:  double.IsNaN(newValue.Right)  ? oldValue.Right  : newValue.Right,
-        bottom: double.IsNaN(newValue.Bottom) ? oldValue.Bottom : newValue.Bottom
-      );
-    }
-
-    private static object GetConsolidatedValue(StyledElement control, AvaloniaProperty property, object value)
+    private static object GetConsolidatedValue(AvaloniaProperty property, object value)
     {
       return (property, value) switch
       {
-        (StyledProperty<Thickness> p, Thickness t) => Merge(control, p, t),
+        (StyledProperty<Thickness> p, Thickness t) => ReplaceNanWithZero(t),
         _ => value,
       };
     }
 
-    private Setter GetSetter(StyledElement control)
+    private static Thickness ReplaceNanWithZero(Thickness newValue)
     {
-      var setter = this.internalSetter;
-      setter.Value = GetConsolidatedValue(control, this.Property, this.Value);
-      setter.Property = this.Property;
-      return setter;
+      return new Thickness
+      (
+        left:   double.IsNaN(newValue.Left)   ? 0 : newValue.Left,
+        top:    double.IsNaN(newValue.Top)    ? 0 : newValue.Top,
+        right:  double.IsNaN(newValue.Right)  ? 0 : newValue.Right,
+        bottom: double.IsNaN(newValue.Bottom) ? 0 : newValue.Bottom
+      );
     }
-
-    // public IDisposable Apply(Style style, StyledElement control, IObservable<bool> activator)
-    // {
-    //   var setter = GetSetter(control);
-    //   return setter.Apply(style, control, activator);
-    // }
   }
 }
