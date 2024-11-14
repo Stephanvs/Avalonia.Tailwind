e46acf4d61ae6ae08cde00f2a118e1aba41d4f8c
Stephan van Stekelenburg <stephan@hayman.io>
2024-11-14 14:58:11 +0100

    refactor: some new language constructs.
    renamed color namespace to colors

---
+++ src/Avalonia.Tailwind/Colors/DefaultColorPaletteDefinitionProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Avalonia.Tailwind.Colors;
+
+public class DefaultColorPaletteDefinitionProvider()
+  : ColorPaletteDefinitionProvider([
+    ColorPalettes.Gray,
+    ColorPalettes.Red,
+    ColorPalettes.Orange,
+    ColorPalettes.Yellow,
+    ColorPalettes.Green,
+    ColorPalettes.Teal,
+    ColorPalettes.Blue,
+    ColorPalettes.Indigo,
+    ColorPalettes.Purple,
+    ColorPalettes.Pink
+  ]);
